6d0ae3c13a38183280c0d2fd49e07464b950837f
Alireza Farokhi <a.farokhi@asredanesh.com>
2021-08-03 15:43:01 +0430

    initialize project and create chat and message component

---
+++ components/chat-component/Chat.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="hero is-primary is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-12-tablet is-12-desktop is-12-widescreen">
+            <Message @logout="logout" :messages="messages" @send-message="send" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Message from '@/components/chat-component/Message.vue'
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+import Cookies from 'js-cookie'
+
+
+@Component({
+  components: {
+    Message,
+  },
+})
+export default class Chat extends Vue {
+  public ws: any
+  public messages: Array<object> = []
+  public socket: any
+  logout() {
+    Cookies.remove('user')
+    this.$router.push('/')
+  }
+  created() {
+
+  }
+  send(message: string): void {
+
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <Chat />
+</template>
+
+<script lang="ts">
+import Chat from '@/components/chat-component/Chat.vue'
+import { Component, Vue } from 'nuxt-property-decorator'
+@Component({
+  components: {
+    Chat
+  }
+})
+export default class MyStore extends Vue {
+
+}
+</script>
